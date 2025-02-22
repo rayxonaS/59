064fce1c8066432734409818efc3af2ea609f7fb
rayxona <rayxona@gmail.com>
2025-02-22 13:45:30 +0500

    initialize project with basic HTML structure and linked JavaScript file

---
+++ app.js
@@ -0,0 +1,6 @@
+const signupForm = document.querySelector("#signup-form");
+const message = document.querySelector(".message");
+
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
