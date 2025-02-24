7dcf9c23fd402b9354236c6ad8ee37a64d6c40fb
rayxona <rayxona@gmail.com>
2025-02-24 14:27:20 +0500

    add styling and layout enhancements; update app.js with color values

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+const body = document.querySelector("body")
+const container = document.querySelector(".container")
+const colortext = document.querySelector(".color-text")
 
+const values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e" "f"];
